03898f5226daa677d475a96665ffcebd7708ce9a
Takuma-Hiraoka <t-hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2025-05-22 11:33:08 +0900

    fix

--- include/graph_search/graph.h
+++ include/graph_search/graph.h
@@ -11,9 +11,9 @@ namespace graph_search{
     std::shared_ptr<Node> goal() {return this->goal_;}
     int& maxExtendNum() {return this->maxExtendNum_;}
 
-    virtual bool isGoalSatisfied(std::shared_ptr<Node> node);
-    virtual void calcHeuristic(std::shared_ptr<Node> node);
-    virtual std::vector<std::shared_ptr<Node> > gatherAdjacentNodes(std::shared_ptr<Node> extend_node);
+    virtual bool isGoalSatisfied(std::shared_ptr<Node> node) = 0;
+    virtual void calcHeuristic(std::shared_ptr<Node> node) = 0;
+    virtual std::vector<std::shared_ptr<Node> > gatherAdjacentNodes(std::shared_ptr<Node> extend_node) = 0;
   private:
     std::vector<std::shared_ptr<Node> > graph_;
     std::shared_ptr<Node> goal_;
